2ce7ea389776f3e73a44ff76278591be6a698901
Anastasia Gornostaeva <ermine@ermine.pp.ru>
2009-03-25 19:27:27 +0300

    Another look at loops

--- brainfuck.ml
+++ brainfuck.ml
@@ -17,56 +17,65 @@
 
 exception Error of string
 
+let parse str =
+  let rec find_eparen deep i =
+    if i < String.length str then
+      match str.[i] with
+        | '[' -> find_eparen (succ deep) (succ i)
+        | ']' -> if deep = 0 then i else find_eparen (pred deep) (succ i)
+        | _ -> find_eparen deep (succ i)
+    else
+      raise (Error "Unmatched [")
+  in
+  let rec aux_parse ps i =
+    if i < String.length str then
+      match str.[i] with
+        | '[' ->
+            let eparen = find_eparen 0 (succ i) in
+              aux_parse ((i, eparen) :: ps) (succ i)
+        | _ -> aux_parse ps (succ i)
+    else
+      List.rev ps
+  in
+    aux_parse [] 0
+    
 let execute ?(array_size=30000) ?(loops_limit=1000000) code putchar getchar =
   let a = Array.make array_size 0 in
-  let rec skip_loop deep i =
-    if i < String.length code then
-      match code.[i] with
-        | '[' -> skip_loop (succ deep) (succ i)
-        | ']' -> if deep = 0 then i else skip_loop (pred deep) (succ i)
-        | _ -> skip_loop deep (succ i)
-    else
-      i
-  in
-  let rec aux_loop looping iters pointer i =
+  let rec aux_loop ps iters pointer i =
     if i < String.length code then
       match code.[i] with
         | '>' ->
-            aux_loop looping iters (succ pointer) (succ i)
+            aux_loop ps iters (succ pointer) (succ i)
         | '<' ->
-            aux_loop looping iters (pred pointer) (succ i)
+            aux_loop ps iters (pred pointer) (succ i)
         | '+' ->
             a.(pointer) <- succ a.(pointer);
-            aux_loop looping iters pointer (succ i)
+            aux_loop ps iters pointer (succ i)
         | '-' ->
             a.(pointer) <- pred a.(pointer);
-            aux_loop looping iters pointer (succ i)
+            aux_loop ps iters pointer (succ i)
         | '.' ->
             putchar (Char.chr a.(pointer));
-            aux_loop looping iters pointer (succ i)
+            aux_loop ps iters pointer (succ i)
         | ',' ->
             a.(pointer) <- Char.code (getchar ());
-            aux_loop looping iters pointer (succ i)
+            aux_loop ps iters pointer (succ i)
         | '[' ->
             if a.(pointer) <> 0 then
               if iters < loops_limit then
-                let _, newpointer = aux_loop true iters pointer (succ i) in
-                  aux_loop looping (succ iters) newpointer i
+                aux_loop ps iters pointer (succ i)
               else
                 raise (Error "Iteration limit exceed")
             else
-              let i = skip_loop 0 (succ i) in
-                aux_loop looping iters pointer (succ i)
+              let (_, i) = List.hd ps in
+                aux_loop (List.tl ps) iters pointer (succ i)
         | ']' ->
-            if looping then
-              looping, pointer
-            else
-              raise (Error "Unmatched ]")
+            let (i, _) = List.hd ps in
+              aux_loop ps iters pointer i
         | _ ->
-            aux_loop looping iters pointer (succ i)
+            aux_loop ps iters pointer (succ i)
     else
-      looping, pointer
+      ()
   in
-  let looping, _ = aux_loop false 0 0 0 in
-    if looping then
-      raise (Error "Unmatched [")
+  let ps = parse code in
+    aux_loop ps 0 0 0
