306fac56cbb92bfa8ecf28f1437e792c3314b380
Anastasia Gornostaeva <ermine@ermine.pp.ru>
2009-03-26 01:21:17 +0300

    Removed currying

--- brainfuck.ml
+++ brainfuck.ml
@@ -23,42 +23,41 @@ let execute ?(array_size=30000) ?(loops_limit=1000000)
     str user_putchar user_getchar =
   let a = Array.make array_size 0 in
   let rec aux_loop (iters:int) (loops:(int * int) list) (pointer:int)
-      (i:int)
-      (code:code array) : unit =
+      (i:int) (code:code array) =
     if i < Array.length code then
       code.(i) iters loops pointer i code
     else
       ()
-  and plus iters loops pointer i =
+  and plus iters loops pointer i code =
     a.(pointer) <- succ a.(pointer);
-    aux_loop iters loops pointer (succ i)
-  and minus iters loops pointer i =
+    aux_loop iters loops pointer (succ i) code
+  and minus iters loops pointer i code =
     a.(pointer) <- pred a.(pointer);
-    aux_loop iters loops pointer (succ i)
-  and shift_left iters loops pointer i =
-    aux_loop iters loops (pred pointer) (succ i)
-  and shift_right iters loops pointer i =
-    aux_loop iters loops (succ pointer) (succ i)
-  and putchar iters loops pointer i =
+    aux_loop iters loops pointer (succ i) code
+  and shift_left iters loops pointer i code =
+    aux_loop iters loops (pred pointer) (succ i) code
+  and shift_right iters loops pointer i code =
+    aux_loop iters loops (succ pointer) (succ i) code
+  and putchar iters loops pointer i code =
     user_putchar (Char.chr a.(pointer));
-    aux_loop iters loops pointer (succ i)
-  and getchar iters loops pointer i =
+    aux_loop iters loops pointer (succ i) code
+  and getchar iters loops pointer i code =
     a.(pointer) <- Char.code (user_getchar ());
-    aux_loop iters loops pointer i
-  and start_loop iters loops pointer i =
+    aux_loop iters loops pointer i code
+  and start_loop iters loops pointer i code =
     if a.(pointer) <> 0 then
       if iters < loops_limit then
-        aux_loop (pred iters) loops pointer (succ i)
+        aux_loop (pred iters) loops pointer (succ i) code
       else
         raise (Error "Iteration limit exceed")
     else
       let (_, i) = List.hd loops in
-        aux_loop iters (List.tl loops) pointer i
-  and end_loop iters loops pointer i =
+        aux_loop iters (List.tl loops) pointer i code
+  and end_loop iters loops pointer i code =
     let (i, _) = List.hd loops in
-      aux_loop iters loops pointer i
-  and ign iters loops pointer i =
-    aux_loop iters loops pointer (succ i)
+      aux_loop iters loops pointer i code
+  and ign iters loops pointer i code =
+    aux_loop iters loops pointer (succ i) code
   in
   let rec find_eparen deep i =
     if i < String.length str then
